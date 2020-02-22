9c03b765bb9f2c759c394682e74b5848e11967c5
Don Mallory <dmm333@gmail.com>
2020-02-22 22:00:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,25 +1,7 @@
 require "YAML"
 
 def game_hash
-  game = {:home =>{
-              :team_name => "Brooklyn Nets", :colors => ["Black", "White"],
-                :players => [{:player_name => "", :number => , :shoe => }
-
-
-                ]
-
-
-    }, :away =>{
-              :team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"],
-                :players => [
-
-
-                ]
-
-
-
-  }
-}
+  game = YAML.load("stats.yml")
 
 
   game
