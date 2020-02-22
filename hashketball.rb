6ce0254a1ec8807166a33f9bf65133548a848faa
Don Mallory <dmm333@gmail.com>
2020-02-22 22:31:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,3 +3,13 @@ require "yaml"
 def game_hash
   YAML.load_file("stats.yml")
 end
+
+def num_points_scored(player_name)
+  stats = game_hash
+
+  stats.each_pair {|x, team|
+        team[:players].each {|player|
+          return player[:points] if player[:player_name] == player_name
+        }
+  }
+end
