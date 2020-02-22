ef9a3999c42e819fe91cb0643210e075e72ea69c
Don Mallory <dmm333@gmail.com>
2020-02-22 21:59:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,22 @@
+require "YAML"
+
 def game_hash
   game = {:home =>{
-              :team_name => "Brooklyn Nets", :colors => ["Black", "White"]
+              :team_name => "Brooklyn Nets", :colors => ["Black", "White"],
+                :players => [{:player_name => "", :number => , :shoe => }
+
+                  
+                ]
+                  
+                
     }, :away =>{
-              :team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"]
+              :team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"],
+                :players => [
+
+                  
+                ]
+                  
+                
 
   }
 }
