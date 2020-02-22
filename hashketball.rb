a3517ac14357d86491f6bb33db2b8c42232e6f40
Don Mallory <dmm333@gmail.com>
2020-02-22 22:34:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -23,3 +23,11 @@ def shoe_size(player_name)
         }
   }
 end
+
+def team_colors(team_name)
+  stats = game_hash
+
+  stats.each_pair {|x, team|
+    return team[:colors] if team[:team_name] == team_name
+  }
+end
