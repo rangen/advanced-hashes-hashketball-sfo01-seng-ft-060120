e3d446e9726e19ba857c14054399a500192d3755
Don Mallory <dmm333@gmail.com>
2020-02-22 22:39:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -43,8 +43,14 @@ end
 
 def player_numbers(team_name)
   stats = game_hash
-
+  jerseys = []
+  
   stats.each_pair {|x, team|
-    return team[:colors] if team[:team_name] == team_name
+    if team[:team_name] == team_name
+      team[:players].each {|player|
+          jerseys << player[:number]
+      }
+    end
   }
+  jerseys
 end
